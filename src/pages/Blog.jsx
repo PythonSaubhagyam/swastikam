00d4c70b81f1c6cc2fd094d599372d38786b634b
dixi1365 <dixi@saubhagyam.org>
2024-08-13 19:08:38 +0530

    changes : design

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -44,130 +44,130 @@ function Blog() {
   return (
     <>
       <Navbar />
-      <Container maxW="container.xl" my={10}>
-        <Heading fontWeight={300} lineHeight={1.25} mb={4} ml={9}>
-          {blogData?.title}
-        </Heading>
-        <Text color="gray.500" fontSize={"sm"} ml={9}>
-          <TimeIcon me={2} />{" "}
-          {blogData?.published_at &&
-            new Intl.DateTimeFormat("en-CA", {
-              dateStyle: "long",
-              timeZone: "Asia/Kolkata",
-            }).format(new Date(blogData?.published_at))}
-        </Text>
-        <Image
-          src={blogData?.banner_url}
-          w="100%"
-          // maxH="400px"
-          my={3}
-          objectFit={"cover"}
-          objectPosition={"center"}
-          mb={8}
-          px={9}
-        />
-        <Flex
-          gap={10}
-          justify="space-between"
-          pb={6}
-          borderBottom={"1px"}
-          borderColor="gray.300"
-          direction={{ base: "column", md: "row" }}
-        >
-          <Box
-            w={{ base: "100%", lg: "70%" }}
-            fontSize="xl"
-            whiteSpace={"pre-line"}
-            lineHeight={1.5}
-            textAlign="justify"
-            dangerouslySetInnerHTML={{
-              __html: dompurify.sanitize(blogData?.content),
-            }}
-          />
-          <Flex direction={"column"} gap={10}>
-            <Box borderLeft={"1px"} borderColor={"brand.900"} p={3}>
-              <Image
-                src={
-                  "https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/images/suryan organic inline.png"
-                }
-              />
-              <Text fontSize={"xs"} color="gray.500" mt={2}>
-                {blogData?.published_at &&
-                  new Intl.DateTimeFormat("en-CA", {
-                    dateStyle: "long",
-                    timeZone: "Asia/Kolkata",
-                  }).format(new Date(blogData?.published_at))}
-              </Text>
-            </Box>
-            <Box>
-              <Heading
-                size="xs"
-                borderBottom={"1px"}
-                borderColor="gray.300"
-                pb={4}
-              >
-                SHARE THIS POST
-              </Heading>
-              <ButtonGroup p={4} gap={2}>
-                <a
-                  href="https://www.facebook.com/soseorganic/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <IconButton
-                    isRound
-                    border="1px"
-                    borderColor={"gray.300"}
-                    icon={<Icon as={FaFacebookSquare} color="facebook.600" />}
-                  />
-                </a>
-                <a href="" target="_blank" rel="noopener noreferrer">
-                  <IconButton
-                    isRound
-                    border="1px"
-                    borderColor={"gray.300"}
-                    icon={<Icon as={FaTwitter} color="twitter.500" />}
-                  />
-                </a>
-                <a href="" target="_blank" rel="noopener noreferrer">
-                  <IconButton
-                    isRound
-                    border="1px"
-                    borderColor={"gray.300"}
-                    icon={<Icon as={FaLinkedinIn} color="linkedin.900" />}
-                  />
-                </a>
-              </ButtonGroup>
-            </Box>
-          </Flex>
-        </Flex>
-        <Box
-          mt={4}
-          ms="auto"
-          display={nextPost ? "flex" : "none"}
-          gap={4}
-          align="end"
-          width="fit-content"
-          maxW={{ base: "95vw", lg: "50vw" }}
-        >
-          <Box as={Link} to={`/blogs/${nextPost?.id}`}>
-            <Text
-              bg="brand.500"
-              color="white"
-              fontSize="sm"
-              px={2}
-              // w="fit-content"
-              ms="auto"
-            >
-              Read Next
-            </Text>
-            <Text fontSize={{ base: "md", lg: "2xl" }}>{nextPost?.title}</Text>
-          </Box>
-          <AspectRatio w="300px" ratio={16 / 9}>
-            <Image src={nextPost?.banner_url} objectFit="cover" />
-          </AspectRatio>
-        </Box>
-      </Container>
+      <Container maxW="6xl" my={10}>
+			<Heading fontWeight={300} lineHeight={1.25} mb={4} ml={9}>
+			  {blogData?.title}
+			</Heading>
+			<Text color="gray.500" fontSize={"sm"} ml={9}>
+			  <TimeIcon me={2} />{" "}
+			  {blogData?.published_at &&
+				new Intl.DateTimeFormat("en-CA", {
+				  dateStyle: "long",
+				  timeZone: "Asia/Kolkata",
+				}).format(new Date(blogData?.published_at))}
+			</Text>
+			<Image
+			  src={blogData?.banner_url}
+			   w="100%"
+			  //  maxH="400px"
+			  my={3}
+			  objectFit={"cover"}
+			  objectPosition={"center"}
+			  mb={8}
+			  display={"block"}
+			  m={"auto"}
+			  p={8}
+			  maxWidth="100%"
+			/>
+			<Flex
+			  gap={16}
+			  justify="space-between"
+			  pb={6}
+			  borderBottom={"1px"}
+			  borderColor="gray.300"
+			  direction={{ base: "column", md: "row" }}
+			  textAlign="center"
+			  justifyContent="center"
+			>
+			  <Box
+				w={{ base: "100%", lg: "70%" }}
+				fontSize="xl"
+				whiteSpace={"pre-line"}
+				lineHeight={1.5}
+				textAlign="justify"
+				dangerouslySetInnerHTML={{
+				  __html: dompurify.sanitize(blogData?.content),
+				}}
+			  />
+			  <Flex direction={"column"} gap={16}>
+				<Box borderLeft={"1px"} borderColor={"brand.900"} p={3} >
+				  <Image
+					src={"https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/images/suryan organic inline.png"}
+				  />
+				  <Text fontSize={"xs"} color="gray.500" mt={2}> 
+					{blogData?.published_at &&
+					  new Intl.DateTimeFormat("en-CA", {
+						dateStyle: "long",
+						timeZone: "Asia/Kolkata",
+					  }).format(new Date(blogData?.published_at))}
+				  </Text>
+				</Box>
+				<Box>
+				  <Heading
+					size="xs"
+					borderBottom={"1px"}
+					borderColor="gray.300"
+					pb={4}
+				  
+				  >
+					SHARE THIS POST
+				  </Heading>
+				   <ButtonGroup p={4} gap={2}>
+					<a href="https://www.facebook.com/soseorganic/" target="_blank" rel="noopener noreferrer">
+					  <IconButton
+						isRound
+						border="1px"
+						borderColor="gray.300"
+						icon={<Icon as={FaFacebookSquare} color="facebook.600" />}
+					  />
+					</a>
+					<a href="" target="_blank" rel="noopener noreferrer">
+					  <IconButton
+						isRound
+						border="1px"
+						borderColor="gray.300"
+						icon={<Icon as={FaTwitter} color="twitter.500" />}
+					  />
+					</a>
+					<a href="" target="_blank" rel="noopener noreferrer">
+					  <IconButton
+						isRound
+						border="1px"
+						borderColor="gray.300"
+						icon={<Icon as={FaLinkedinIn} color="linkedin.900" />}
+					  />
+					</a>
+				  </ButtonGroup>
+				</Box>
+			  </Flex>
+			</Flex>
+			<Box
+			  mt={4}
+			  ms="auto"
+			  display={nextPost ? "flex" : "none"}
+			  gap={4}
+			  align="end"
+			  width="fit-content"
+			  maxW={{ base: "95vw", lg: "50vw" }}
+			>
+			  <Box as={Link} to={`/blogs/${nextPost?.id}`}>
+				<Text
+				  bg="brand.500"
+				  color="white"
+				  fontSize="sm"
+				  px={2}
+				  // w="fit-content"
+				  ms="auto"
+				>
+				  Read Next
+				</Text>
+				<Text fontSize={{ base: "md", lg: "2xl" }}>{nextPost?.title}</Text>
+			  </Box>
+			  <AspectRatio w="300px" ratio={16 / 9}>
+				<Image src={nextPost?.banner_url} objectFit="cover" />
+			  </AspectRatio>
+			</Box>
+		  </Container>
       <Footer />
     </>
   );

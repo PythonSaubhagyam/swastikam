f5fd9b1e6234c07d306ccd43daadb34387064da7
Snehal2930 <134259593+Snehal2930@users.noreply.github.com>
2024-10-07 18:11:30 +0530

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -277,7 +277,7 @@ export default function Navbar() {
 
   useEffect(() => {
     //CheckOrSetUDID();
-    getMegaCategories();
+    //getMegaCategories();
   }, []);
 
   const getMegaCategories = async () => {
@@ -353,6 +353,7 @@ export default function Navbar() {
 
     if (response.data.status === true) {
       setCategories(response.data.categories);
+      setMegaCategories(response.data.categories);
       setTopCategory(mergeArraysById(mainLinks, response.data.categories));
     }
   };
